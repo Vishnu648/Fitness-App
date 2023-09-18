319c5b2a43b19b748705a697e2ddb05901699a4b
VishnuVS <vishnusenthil222@gmail.com>
2023-09-18 22:04:09 +0530

    hero set

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,11 @@ import Banner from '../assets/Banner.png'
 const Hero = () => {
   return (
     <div className='h-[614px] flex justify-between'>
-      <img src={Banner} alt="banner" className='h-[674px] w-120 relative top-[-60px] left-[-2rem]'/>
-      <div className='border border-white flex-1 flex flex-col justify-center items-center'>
-      <h1 className='text-blue-300 font-bold text-2xl'>
-        YOUR BODY CAN DO IT. <br/>
-        IT'S TIME TO CONVINCE YOUR MIND
+      <img src={Banner} alt="banner" className='h-[674px] w-[35%] relative top-[-60px] left-[-2rem]'/>
+      <div className='flex-1 flex flex-col justify-center items-center'>
+      <h1 className='text-blue-300 font-bold text-5xl px-8 flex flex-col items-center leading-[55px]'>
+        <span>YOUR BODY</span> <span className='text-red-600'>CAN DO IT<span className='text-blue-300'>.</span></span>
+        <span>IT'S TIME TO</span>  CONVINCE YOUR <span className='text-red-600'>MIND</span>
       </h1>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,16 @@ const Navbar = () => {
   return (
     <div className='w-full h-[60px] bg-[#010511] flex flex-row-reverse px-8'>
       <div className='w-[25%] flex flex-row-reverse justify-between items-center '>
-        <img className='w-10 h-10' src={Logo} alt="logo" />
-          <a href="#exercise">
-            EXERCISE
-          </a>
-          <a href="#">
-            HOME
-          </a>
+        <a href="#">
+          <img className='w-10 h-10' src={Logo} alt="logo" />
+        </a>
+
+        <a href="#exercise">
+          EXERCISE
+        </a>
+        <a href="#">
+          HOME
+        </a>
       </div>
     </div>
   )
